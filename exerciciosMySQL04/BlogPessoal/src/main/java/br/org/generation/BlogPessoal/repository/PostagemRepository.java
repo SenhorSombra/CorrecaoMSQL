b4788fd370c021ef57e74514fba17de05819c13e
Vagner <vagner-bw@hotmail.com>
2022-08-01 14:40:31 -0300

    Adicionado metodo post e put

--- exerciciosMySQL04/BlogPessoal/src/main/java/br/org/generation/BlogPessoal/repository/PostagemRepository.java
+++ exerciciosMySQL04/BlogPessoal/src/main/java/br/org/generation/BlogPessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package br.org.generation.BlogPessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.org.generation.BlogPessoal.model.Postagem;
@@ -9,6 +12,8 @@ import br.org.generation.BlogPessoal.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
 	
+	public List<Postagem>findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	//trazer da Lista da Tabela, Pelo Titulo e Ignore SE é maiuscula e minuscula e Liste todos os titulos com os titulos iguais
 	
 
 }

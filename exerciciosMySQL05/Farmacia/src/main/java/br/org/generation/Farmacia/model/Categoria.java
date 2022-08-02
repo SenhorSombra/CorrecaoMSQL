723e529dc5bf3a0ab27909d8ab7a3b963df796a4
Vagner <vagner-bw@hotmail.com>
2022-08-02 16:56:21 -0300

    Reproduzindo Banco de Dados Farmacia

---
+++ exerciciosMySQL05/Farmacia/src/main/java/br/org/generation/Farmacia/model/Categoria.java
@@ -0,0 +1,86 @@
+package br.org.generation.Farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name="tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private Long descricao;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private String medicamentos;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private String higieneP;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private String medicamentosG;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private String saudeEBemEstar;
+	@NotBlank
+	@Size(min=5, max= 500)
+	private String Beleza;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(Long descricao) {
+		this.descricao = descricao;
+	}
+	public String getMedicamentos() {
+		return medicamentos;
+	}
+	public void setMedicamentos(String medicamentos) {
+		this.medicamentos = medicamentos;
+	}
+	public String getHigieneP() {
+		return higieneP;
+	}
+	public void setHigieneP(String higieneP) {
+		this.higieneP = higieneP;
+	}
+	public String getMedicamentosG() {
+		return medicamentosG;
+	}
+	public void setMedicamentosG(String medicamentosG) {
+		this.medicamentosG = medicamentosG;
+	}
+	public String getSaudeEBemEstar() {
+		return saudeEBemEstar;
+	}
+	public void setSaudeEBemEstar(String saudeEBemEstar) {
+		this.saudeEBemEstar = saudeEBemEstar;
+	}
+	public String getBeleza() {
+		return Beleza;
+	}
+	public void setBeleza(String beleza) {
+		Beleza = beleza;
+	}
+	
+	
+	
+	
+
+}
